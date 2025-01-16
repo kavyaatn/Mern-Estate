e0ba64f07599e6ac5030faf83d4fb5d3fa675113
kavya <kavyanagaraj15@gmail.com>
2025-01-16 22:11:26 +0530

    authentication error

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,5 +1,28 @@
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 export const test =(req,res)=>{
     res.json({
         message: 'hiii'
     });
 }
+export const UpdateUser = async(req,res,next) =>{
+    if(req.user.id !== req.params.id) return next(errorHandler(401,'cannot update'))
+    try{
+    if(req.body.password){
+        req.body.password = bcryptjs.hashSync(req.body.password, 10)
+    }
+    const UpdateUser = await User.findByIDAndUpdate(req.params.id,{
+        $set:{
+            username: req.body.username,
+            email: req.body.email,
+            password:req.body.password,
+            avatar: req.body.avatar
+        }
+    },{new:true})
+    const {password, ...rest} =UpdateUser._doc;
+    res.status(200).json(rest)
+}catch(error){
+    next(error)
+}
+};
